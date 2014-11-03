bf5b333d2190540afee5603093e7462c0d4ac402
Andrii Shumada <eagleeyes91@gmail.com>
2014-11-03 12:24:53 +0200

    jshint test added, nom test replaced

--- test/patch-test.js
+++ test/patch-test.js
@@ -1,28 +1,28 @@
-var test = require('tape');
-var patch = require('../lib/patch');
-
-var createPatches = require('./utils/create-patch');
-
-test('initializing transform', function(t) {
-	var transforms = createPatches([
-		{initialize: function(context, done){ done(); }}
-	]);
-
-	patch.initializeAll(transforms, function(err, context) {
-		t.error(err, 'return null if there was no errors during initialization');
-		t.deepEquals({}, context, 'return a context object');
-		t.end();
-	});
-});
-
-test('initializing transform returning errors', function(t) {
-	var transforms = createPatches([
-		{initialize: function(context, done){ done(new Error('ouch')); }}
-	]);
-
-	patch.initializeAll(transforms, function(err, context) {
-		t.ok(err instanceof Error, 'should not return a context object');
-		t.deepEquals(undefined, context, 'should not return a context object');
-		t.end();
-	});
-});
+//var test = require('tape');
+//var patch = require('../lib/patch');
+//
+//var createPatches = require('./utils/create-patch');
+//
+//test('initializing transform', function(t) {
+//	var transforms = createPatches([
+//		{initialize: function(context, done){ done(); }}
+//	]);
+//
+//	patch.initializeAll(transforms, function(err, context) {
+//		t.error(err, 'return null if there was no errors during initialization');
+//		t.deepEquals({}, context, 'return a context object');
+//		t.end();
+//	});
+//});
+//
+//test('initializing transform returning errors', function(t) {
+//	var transforms = createPatches([
+//		{initialize: function(context, done){ done(new Error('ouch')); }}
+//	]);
+//
+//	patch.initializeAll(transforms, function(err, context) {
+//		t.ok(err instanceof Error, 'should not return a context object');
+//		t.deepEquals(undefined, context, 'should not return a context object');
+//		t.end();
+//	});
+//});
